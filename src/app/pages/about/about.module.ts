6d191f00f609c16df04efb7711aaa09086c63ea8
Michal Wiraszka <michal@wiraszka.com>
2022-05-21 16:12:53 -0400

    Create a basic about us page

--- src/app/pages/about/about.module.ts
+++ src/app/pages/about/about.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { ClarityModule } from '@clr/angular';
 
 import { AboutComponent } from '@app/pages/about';
 
 @NgModule({
   declarations: [AboutComponent],
-  imports: [ClarityModule, CommonModule],
+  imports: [ClarityModule, CommonModule, RouterModule],
 })
 export class AboutModule {}

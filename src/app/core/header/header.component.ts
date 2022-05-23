5c16b430985d80788abc8f5a8afcdd6e15a59e2d
Michal Wiraszka <michal@wiraszka.com>
2022-05-23 01:02:27 -0400

    Re-vamp site header; update branding SVGs

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,26 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { ClarityIcons } from '@cds/core/icon';
-import { IconShapeTuple } from '@cds/core/icon/interfaces/icon.interfaces';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'lcc-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
-  lccIcon: IconShapeTuple = [
-    'lcc-logo',
-    `
-      <svg viewBox="0 0 45 45">
-        <use xlink:href="../../assets/lcc-logo.svg#logo">
-        </use>
-      </svg>
-    `,
-  ];
-
-  constructor() {}
-
-  ngOnInit(): void {
-    ClarityIcons.addIcons(this.lccIcon);
-  }
-}
+export class HeaderComponent {}

acec0e066ae6913c7ca70d07859eb5d5f6cdc3f5
mwiraszka <michal@wiraszka.com>
2024-12-13 04:09:12 +0200

    feat: Add article appearances counter underneath each image in Image Explorer component; various touch-ups

--- src/app/components/image-explorer/image-explorer.component.ts
+++ src/app/components/image-explorer/image-explorer.component.ts
@@ -1,4 +1,5 @@
-import { Observable } from 'rxjs';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import * as uuid from 'uuid';
 
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
@@ -6,8 +7,9 @@ import { Component, OnInit } from '@angular/core';
 import { FormatBytesPipe } from '@app/pipes/format-bytes.pipe';
 import { FormatDatePipe } from '@app/pipes/format-date.pipe';
 import { ImagesService } from '@app/services';
-import { Image } from '@app/types';
+import { Id, Image } from '@app/types';
 
+@UntilDestroy()
 @Component({
   selector: 'lcc-image-explorer',
   templateUrl: './image-explorer.component.html',
@@ -15,11 +17,28 @@ import { Image } from '@app/types';
   imports: [CommonModule, FormatBytesPipe, FormatDatePipe],
 })
 export class ImageExplorer implements OnInit {
-  images$?: Observable<Image[]>;
+  readonly PLACEHOLDER_IMAGE: Image = {
+    articleAppearances: 0,
+    dateUploaded: new Date().toISOString(),
+    id: uuid.v4(),
+    presignedUrl: '',
+    size: 0,
+  };
+
+  images: Image[] = Array(25).fill(this.PLACEHOLDER_IMAGE);
 
   constructor(private imagesService: ImagesService) {}
 
   ngOnInit(): void {
-    this.images$ = this.imagesService.getThumbnailImages();
+    this.imagesService
+      .getThumbnailImages()
+      .pipe(untilDestroyed(this))
+      .subscribe(images => {
+        this.images = images;
+      });
+  }
+
+  onSelectImage(id: Id): void {
+    // TODO: set up callback/emitter mechanism in overlay service
   }
 }
--- src/app/types/image.model.ts
+++ src/app/types/image.model.ts
@@ -1,6 +1,7 @@
 import { Id, IsoDate, Url } from './core.model';
 
 export interface Image {
+  articleAppearances: number;
   dateUploaded: IsoDate;
   id?: Id;
   presignedUrl: Url;

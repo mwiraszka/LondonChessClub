de81464c2fd9c34ae67c991491d1851e62f8afc4
mwiraszka <michal@wiraszka.com>
2025-02-05 00:00:30 -0500

    chore: Remove slash from prod API base url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   googleMapsApiKey: 'AIzaSyAuLCJPCzFJXPWYUPM61Txue8PeJabI6DI',
-  lccApiBaseUrl: 'https://api.londonchess.ca/v1/',
+  lccApiBaseUrl: 'https://api.londonchess.ca/v1',
 };

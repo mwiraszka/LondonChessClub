5afae425b403d9c752cd3391c578b21d25949a1c
mwiraszka <michal@wiraszka.com>
2026-06-22 00:47:21 +0200

    chore: set up dev, preview, and production vercel environments

--- frontend/src/environments/environment.preview.ts
+++ frontend/src/environments/environment.preview.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   clerkPublishableKey: 'pk_live_Y2xlcmsubG9uZG9uY2hlc3MuY2Ek',
   googleMapsApiKey: 'AIzaSyCeuTgq4qe2k8obnbYWrwWsG91B6aDUSA0',
-  lccApiBaseUrl: 'https://api.londonchess.ca/v1',
+  lccApiBaseUrl: 'https://api-preview.londonchess.ca/v1',
   sentryDsn:
     'https://15ca529d0b2ea8ff491edc33e2f234fc@o4511323014234112.ingest.de.sentry.io/4511323126956112',
 };
